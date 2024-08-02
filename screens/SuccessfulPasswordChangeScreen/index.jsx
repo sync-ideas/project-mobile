13ba75d9b386f174d2e9807c35ec53574be3f4ec
GonzaloMonteodorisio <gonzalomonteodorisio@gmail.com>
2024-08-02 19:02:35 -0300

    Update SuccessfulPasswordChangeScreen styles

--- screens/SuccessfulPasswordChangeScreen/index.jsx
+++ screens/SuccessfulPasswordChangeScreen/index.jsx
@@ -3,20 +3,28 @@ import {
   View, 
   Text, 
   TouchableOpacity, 
+  Image,
 } from "react-native";
+import Logo from "../../assets/logo.png";
 import styles from "./styles";
 
 const SuccessfulPasswordChangeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Haz cambiado exitosamente tu contraseña, ahora puedes volver a iniciar sesión.</Text>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
-        <Text>Iniciar sesión</Text>
-      </TouchableOpacity>
+      <View style={styles.logoContainer}>
+        <Image source={Logo} height={106} width={106} />
+      </View>
+      <View style={styles.formContainer}>
+        <Text style={styles.text}>Haz cambiado exitosamente tu contraseña, ahora puedes volver a iniciar sesión.</Text>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
+          <Text style={styles.buttonText}>Iniciar sesión</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default SuccessfulPasswordChangeScreen;
+
 
 
--- screens/SuccessfulPasswordChangeScreen/styles.js
+++ screens/SuccessfulPasswordChangeScreen/styles.js
@@ -4,13 +4,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: "#fff",
+  },
+  logoContainer: {
+    marginBottom: 60,
+  },
+  formContainer: {
+    alignItems: "left",
+    width: "100%",
+    marginLeft: 82,
   },
   button: {
-    marginTop: 10,
-    backgroundColor: '#6200EE',
-    padding: 10
-  }
+    backgroundColor: "#63318a",
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    width: "80%",
+    alignItems: "center",
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  text: {
+    width: "80%",
+    textAlign: "center",
+    marginBottom: 20,
+  },
 });
 
 export default styles;
